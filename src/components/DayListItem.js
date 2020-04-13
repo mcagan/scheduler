ae32aacc3f46198776c2510bc04989f0ae52b6e5
Melis Cagan <meliscagan@Meliss-MacBook-Air.local>
2020-04-13 16:17:10 -0400

    implement function for different messages based on remaining spots

---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const formatSpots = function () {
+    if (props.spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>;
+    }
+    if (props.spots === 1) {
+      return <h3 className="text--light">{props.spots} spot remaining</h3>;
+    }
+    return <h3 className="text--light">{props.spots} spots remaining</h3>;
+  };
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      {formatSpots()}
+    </li>
+  );
+}
