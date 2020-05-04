ca4885dc1d3ad5ca00e8489ed18fd4e16044a896
Melis Cagan <meliscagan@Meliss-MacBook-Air.local>
2020-05-04 15:09:11 -0400

    fix test for form

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  getByAltText,
+} from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -46,9 +51,12 @@ describe("Form", () => {
 
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
+    const {
+      getByText,
+      getByPlaceholderText,
+      queryByText,
+      getByAltText,
+    } = render(<Form interviewers={interviewers} onSave={onSave} />);
 
     fireEvent.click(getByText("Save"));
 
@@ -59,12 +67,14 @@ describe("Form", () => {
       target: { value: "Lydia Miller-Jones" },
     });
 
+    fireEvent.click(getByAltText("Sylvia Palmer"));
+
     fireEvent.click(getByText("Save"));
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
   it("calls onCancel and resets the input field", () => {
     const onCancel = jest.fn();
